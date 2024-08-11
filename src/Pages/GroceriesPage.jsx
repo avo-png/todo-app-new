8a8585091a813785042497da2b11e76a55f9895d
dtroy345 <59236999+dtroy345@users.noreply.github.com>
2024-08-10 22:35:26 -0400

    slight update

--- src/Pages/GroceriesPage.jsx
+++ src/Pages/GroceriesPage.jsx
@@ -1,7 +1,59 @@
+import { useEffect, useState } from "react";
+import { NewTodoForm } from "../Components/NewTodoForm";
+// // import "./style.css";
+import { TodoList } from "../Components/TodoList";
+
 export const GroceriesPage = () => {
+	const [todos, setTodos] = useState(() => {
+		const localValue = localStorage.getItem("ITEMS");
+		if (localValue == null) return [];
+
+		return JSON.parse(localValue);
+	});
+
+	useEffect(() => {
+		localStorage.setItem("ITEMS", JSON.stringify(todos));
+	}, [todos]);
+
+	function addTodo(title) {
+		setTodos((currentTodos) => {
+			return [
+				...currentTodos,
+				{ id: crypto.randomUUID(), title, completed: false },
+			];
+		});
+	}
+
+	function toggleTodo(id, completed) {
+		setTodos((currentTodos) => {
+			return currentTodos.map((todo) => {
+				if (todo.id === id) {
+					return { ...todo, completed };
+				}
+
+				return todo;
+			});
+		});
+	}
+
+	function deleteTodo(id) {
+		setTodos((currentTodos) => {
+			return currentTodos.filter((todo) => todo.id !== id);
+		});
+	}
+
 	return (
 		<>
-			<h1> groceries page </h1>
+			<h1>Groceries</h1>
+			<div className="main-notes">
+				<NewTodoForm onSubmit={addTodo} />
+				<h1 className="header">Todo List</h1>
+				<TodoList
+					todos={todos}
+					toggleTodo={toggleTodo}
+					deleteTodo={deleteTodo}
+				/>
+			</div>
 		</>
 	);
 };
